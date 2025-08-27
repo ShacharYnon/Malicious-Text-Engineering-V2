26895ed484fb7b8ed8b2ed734b90bd708a9d526c
Eclipse <Eclips77@users.noreply.github.com>
2025-08-27 12:52:24 +0300

    Add Publisher class for Kafka message publishing

--- services/retriver/config.py
+++ services/retriver/config.py
@@ -1,8 +1,10 @@
 import os
+from typing import List
 
 MONGODB_URI = os.getenv("MONGODB_URI" ,"mongodb+srv://IRGC_NEW:iran135@cluster0.6ycjkak.mongodb.net/")
 MONGODB_DATABASE = os.getenv("MONGODB_DATABASE" ,"IranMalDB")
 MONGODB_COL = os.getenv("MONGODB_COL" ,"tweets")
+KAFKA_BOOTSTRAP: List[str] = os.getenv("KAFKA_BOOTSTRAP", "kafka:9092").split(",")
 
 
 
