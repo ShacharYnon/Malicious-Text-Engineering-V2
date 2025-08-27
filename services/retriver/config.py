967bb00f80021574e6f84f30259dc18a6ec6743c
Shachar <shar535909@gmail.com>
2025-08-27 11:35:10 +0300

    Add MongoDB configuration to retriver service

---
+++ services/retriver/config.py
@@ -0,0 +1,11 @@
+import os
+
+MONGODB_URI = os.getenv("MONGODB_URI" ,"mongodb+srv://IRGC_NEW:iran135@cluster0.6ycjkak.mongodb.net/")
+MONGODB_DATABASE = os.getenv("MONGODB_DATABASE" ,"IranMalDB")
+MONGODB_COL = os.getenv("MONGODB_COL" ,"tweets")
+
+
+
+
+
+
