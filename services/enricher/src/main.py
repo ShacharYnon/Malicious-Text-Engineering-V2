665fb33bbcbfa9d4f10cd3783cf8930c12246900
Eclipse <Eclips77@users.noreply.github.com>
2025-08-28 13:04:04 +0300

    Refactor EnricherService to load weapons list internally and streamline document enrichment process

--- services/enricher/src/main.py
+++ services/enricher/src/main.py
@@ -1,5 +1,5 @@
-from features import SentimentEnhancer
-from weapons_detector import WeaponsDetector
+from .features import SentimentEnhancer
+from .weapons_detector import WeaponsDetector
 import logging
 import os
 from typing import List, Dict, Any
@@ -7,9 +7,25 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class EnricherService:
-    def __init__(self, weapons_list: List[str]):
+    def __init__(self):
         self.sentiment_enhancer = SentimentEnhancer()
+        weapons_list = self._load_weapons()
         self.weapons_detector = WeaponsDetector(weapons_list)
+
+    def _load_weapons(self)->List[str]:
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        weapons_file_path = os.path.join(BASE_DIR, "..", "..", "..", "data", "weapon_list.txt")
+        weaponslist = []
+        try:
+            with open(weapons_file_path, 'r') as file:
+                weaponslist = [line.strip() for line in file if line.strip()]
+            logger.info(f"Loaded {len(weaponslist)} weapons from {weapons_file_path}")
+            return weaponslist
+        except Exception as e:
+            logger.error(f"Error loading weapons list: {e}")
+            raise RuntimeError(f"Error loading weapons list: {e}")
+
+
         
     def enrich(self, documents: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
         """
@@ -22,9 +38,20 @@ class EnricherService:
             List[Dict[str, Any]]: List of enriched documents
         """
         try:
-            docs_with_sentiment = self.sentiment_enhancer.enrich_documents(documents)
-            fully_enriched_docs = self.weapons_detector.detect_weapons(docs_with_sentiment)
+            fully_enriched_docs = self.weapons_detector.detect_weapons(documents)
             return fully_enriched_docs
         except Exception as e:
             logger.error(f"Error enriching documents: {e}")
             raise RuntimeError(f"Error enriching documents: {e}")
+        
+    
+if __name__ == "__main__":
+    sample_docs = [
+        {"_id": 1, "Cleaned_Text": "This is a test text with a gun."},
+        {"_id": 2, "Cleaned_Text": "No weapons here."},
+        {"_id": 3, "Cleaned_Text": "Another text with a knife and a rifle."}
+    ]
+    enricher = EnricherService()
+    enriched_docs = enricher.enrich(sample_docs)
+    for doc in enriched_docs:
+        print(doc)
