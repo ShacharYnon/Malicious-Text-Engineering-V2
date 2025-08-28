a1e69fddc2fc79b0945af5c3ad1f8230e1c633c3
Shachar <shar535909@gmail.com>
2025-08-28 14:57:11 +0300

    Refactor ProcessManager to streamline message processing and remove redundant initialization code

--- services/preprocessor/src/main.py
+++ services/preprocessor/src/main.py
@@ -15,40 +15,10 @@ logger = logging.getLogger(__name__)
 
 
 class ProcessManager:
-<<<<<<< HEAD
+
     """
     Consume RAW_* topics, clean/transform, and publish to PROCESSED_* topics.
     """
-=======
-    def __init__(self):
-        self.publisher = Publisher(config.KAFKA_BOOTSTRAP)
-        self.topic_anti = config.KAFKA_TOPIC_ANTI
-        self.topic_not_anti = config.KAFKA_TOPIC_NOT_ANTI
-        self.data = None
-        self.processor = TextCleaner()
-    
-    def process_and_publish(self):
-        logger.info("Starting message consumption and processing")
-        consumer = Consumer([self.topic_anti ,self.topic_not_anti] ,config.KAFKA_BOOTSTRAP ,config.GROUP_ID) 
-        for message in consumer.consume_messages():
-            processed_anti = []
-            processed_not_anti = []
-            # for doc in message:
-            try:
-                cleaned_text = self.processor.clean_central(message.get("text", ""))
-                message["Cleaned_Text"] = cleaned_text
-                if message.get("Antisemitic", False):
-                    processed_anti.append(message)
-                else:
-                    processed_not_anti.append(message)
-            except Exception as e:
-                    logger.error(f"Error processing document ID {message.get('_id')}: {e}")
-            if processed_anti:
-                self.publisher.publish(self.topic_anti, processed_anti)
-            if processed_not_anti:
-                self.publisher.publish(self.topic_not_anti, processed_not_anti)
-            logger.info(f"Processed and published {len(processed_anti)} antisemitic and {len(processed_not_anti)} non-antisemitic documents")
->>>>>>> 9dadd546b272c9c91943ed1287b8c5beca2a5eff
 
     def __init__(
         self,
